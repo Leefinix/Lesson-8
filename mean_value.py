1a9a6ce131ad8ce60cf8c9982f081388d6aa7df4
Leefinix <joaquinnleo@gmail.com>
2025-01-14 18:55:04 +0700

    mean

---
+++ mean_value.py
@@ -0,0 +1,13 @@
+mean1 = 38
+wrong_number = 36
+correct_number = 56
+total_number = 40
+
+sum = mean1 * total_number
+print("The sum of 40 numbers: ", sum)
+
+sum2 = sum - (wrong_number) + (correct_number)
+print("Corrected Sum", sum2)
+
+mean2 = sum2 / total_number
+print(mean2)
